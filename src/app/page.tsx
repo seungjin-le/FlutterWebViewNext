14030c6c4f0f65c4f5f6751d6eb4bf776db26da0
Lee Seung Jin <dltmdwls154@gmail.com>
2025-03-25 22:53:52 +0900

    Test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ export default function Home() {
   return (
     <div className="bg-n20">
      
-     <div className="text-s900 text-h1 bg-n40">dd asd</div>
-     <div className="text-s900 text-l1 ">asd</div>  
+     <div className="text-s400 text-h1 bg-n40">dd asd</div>
+     <div className="text-s400 text-l1 ">asd</div>  
 
 
     </div>
