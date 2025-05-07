5c73afde220993f48bdfc67f9470a88a01c89b39
Seungjin Lee <dltmdwls154@gmail.com>
2025-05-07 23:33:23 +0900

    Test

--- src/layouts/Section.tsx
+++ src/layouts/Section.tsx
@@ -1,15 +1,10 @@
 import { ReactNode } from 'react'
 
-export default function Section({
-  children,
-  className
-}: {
+interface SectionProps {
   children: ReactNode | ReactNode[]
   className?: string
-}) {
-  return (
-    <div className={`flex-col-start bg-dark10 w-full rounded-[8px] p-[10px] ${className}`}>
-      {children}
-    </div>
-  )
+}
+
+export default function Section({ children, className }: SectionProps) {
+  return <div className={`flex-col-start bg-dark10 w-full rounded-[8px] p-[10px] ${className}`}>{children}</div>
 }
