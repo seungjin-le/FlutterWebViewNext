3f68bca388be47ee96c428fce2fc0665d02c5cd4
Seungjin Lee <dltmdwls154@gmail.com>
2025-05-16 23:15:27 +0900

    Test

--- src/app/buffer/page.tsx
+++ src/app/buffer/page.tsx
@@ -3,10 +3,10 @@ import Link from 'next/link'
 export default function Page() {
   return (
     <div className="relative h-[100vh] w-full">
-      buffer
+      Buffer
       <div className={'absolute top-1/2 left-1/2 h-[50px] w-[90%] -translate-x-1/2 -translate-y-1/2 bg-[red]'}>
         <Link href="/attacker" className={'text-h1 text-white'}>
-          attacker
+          Attacker
         </Link>
       </div>
     </div>
